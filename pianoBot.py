78d368e13532b3ed02550fd9821a8638c79b7330
TheAkashRoy <9akash.roy@gmail.com>
2023-06-19 20:21:04 +0530

    done

---
+++ pianoBot.py
@@ -0,0 +1,28 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+
+# X: 335 Y: 800 RGB: (0,0,0,0)
+# X: 444 Y: 800 RGB: (0,0,0,0)
+# X: 565 Y: 800 RGB: (0,0,0,0)
+# X: 682 Y: 800 RGB: (0,0,0,0)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.07)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+while keyboard.is_pressed('q') == False:
+    if pyautogui.pixel(335,800)[0]==0:
+        click(335,800)
+    if pyautogui.pixel(444,800)[0]==0:
+        click(444,800)
+    if pyautogui.pixel(565,800)[0]==0:
+        click(565,800)
+    if pyautogui.pixel(682,800)[0]==0:
+        click(682,800)
